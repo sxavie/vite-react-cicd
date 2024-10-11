2cc65a7027b6917c5e341c840acb4aaf99d13f08
Santiago Hernandez <tmxsantiago.hernandez@kof.com.mx>
2024-10-11 15:46:22 -0600

    Cambio menor

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dan Yael :P</h1>
+      <h1>Santiago X. :D</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
